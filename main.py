a875dcf0c8cdc41a8273b36b3edeb24b4eca3a39
rharris-nitro <ronan.harris@gonitro.com>
2024-01-11 15:50:16 +0000

    submitting print array

---
+++ main.py
@@ -0,0 +1,9 @@
+def main(): 
+   # initializing an array of integers with predefined values
+    integer_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    # printing array
+    print(integer_array)
+
+if __name__ == "__main__":
+    main()
