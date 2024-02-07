260ec06fb2d4ce0c593ee8430e7334f76d3896dd
rharris-nitro <156085339+rharris-nitro@users.noreply.github.com>
2024-02-07 14:53:03 +0000

    Pre commit hooks exercise (#3)
    Learning about pre-commit hooks: what to do with them and how to use
    them
    
    Link:
    
    [https://medium.com/@anton-k./how-to-set-up-pre-commit-hooks-with-python-2b512290436](url)

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 #               -  Parameters: None
 #               -  Returns: None
 
-def main(): 
-   # initializing an array of integers with predefined values
+
+def main():
+    # initializing an array of integers with predefined values
     integer_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     # printing array
     print(integer_array)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
