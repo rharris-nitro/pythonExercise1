129a423a1111d20a528e461e5f36b208ceedf7e5
rharris-nitro <ronan.harris@gonitro.com>
2024-02-12 17:04:28 +0000

    moved main.py to new 'app' folder

---
+++ app/main.py
@@ -0,0 +1,18 @@
+"""
+Module main: This module contains the main function that initializes an array
+of integers and prints it.
+"""
+
+
+def main():
+    """
+    initializing an array of integers with predefined values
+    """
+    integer_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+    # printing array
+    print(integer_array)
+
+
+if __name__ == "__main__":
+    main()
