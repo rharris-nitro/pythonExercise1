730c7ea09de53abaaaf12e585ea4fded20485887
rharris-nitro <ronan.harris@gonitro.com>
2024-02-15 15:31:51 +0000

    adds constructor and getter to help with importing and to satisfy pylint respectively

--- app/main.py
+++ app/main.py
@@ -4,14 +4,38 @@ of integers and prints it.
 """
 
 
+class Main:
+    """
+    Main class for handling integer arrays.
+    """
+
+    def __init__(self):
+        """
+        Initializes an array of integers with predefined values.
+        """
+        self.integer_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        # self.integer_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+    def print_array_to_the_screen(self):
+        """
+        Prints the initialized array to the screen.
+        """
+        # printing array
+        print(self.integer_array)
+
+    def get_array(self):
+        """
+        Returns the initialized array.
+        """
+        return self.integer_array
+
+
 def main():
     """
-    initializing an array of integers with predefined values
+    Main function for creating Main instance and printing the array.
     """
-    integer_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
-    # printing array
-    print(integer_array)
+    main_instance = Main()
+    main_instance.print_array_to_the_screen()
 
 
 if __name__ == "__main__":
