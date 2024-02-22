3742089e3096f3dd15ef6683110a3772e1f19d89
rharris-nitro <ronan.harris@gonitro.com>
2024-02-22 11:47:36 +0000

    accommodates changes in main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,20 +2,20 @@
 Module test_main: This module contains test cases for the Main class in app.main.
 """
 
-from app.main import Main
+from app.main import Data
 
 
 def test_correct_array():
     """
     Test case to ensure the initialized array is correct.
     """
-    main_instance = Main()
-    assert main_instance.integer_array == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    data = Data()
+    assert data.integer_array == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 def test_incorrect_array():
     """
     Test case to ensure the initialized array is not incorrect.
     """
-    main_instance = Main()
-    assert main_instance.integer_array != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    data = Data()
+    assert data.integer_array != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
