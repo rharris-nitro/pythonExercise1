de6add6040afdc632cad663603a4650936c92d5b
rharris-nitro <ronan.harris@gonitro.com>
2024-04-11 10:52:07 +0100

    fixes debugger and global variable

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,15 +2,14 @@
 Module test_main: This module contains test cases for the Main class in app.main.
 """
 
-from app import main
-
-data = main.Data()
+from app.main import Data
 
 
 def test_correct_array():
     """
     Test case to ensure the initialized array is correct.
     """
+    data = Data()
     assert data.integer_array == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
@@ -18,4 +17,5 @@ def test_incorrect_array():
     """
     Test case to ensure the initialized array is not incorrect.
     """
+    data = Data()
     assert data.integer_array != [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
